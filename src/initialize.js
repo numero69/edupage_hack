a42ad12edf5253461c7473626f8a347310429ae6
numero69 <numerouno693@gmail.com>
2021-12-10 17:50:02 +0100

    Delete src directory

--- src/initialize.js
+++
@@ -1,9 +0,0 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-    const run = async () => {
-        await chrome.tabs.executeScript(null, {
-            code: `showAnswers()`
-        });
-        document.getElementById("title").innerText="Done!";
-    }
-    run();
-});
